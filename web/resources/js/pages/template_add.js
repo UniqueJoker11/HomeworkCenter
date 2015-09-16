16c49158798f7e76f169b624dfa3d9829515c7bd
UniqueJoker11 <xiaochou913>
2015-09-16 11:12:40 +0800

    修改模板上传模块

--- web/resources/js/pages/template_add.js
+++ web/resources/js/pages/template_add.js
@@ -3,29 +3,41 @@
  */
 $(function () {
     //初始化上传文件
-    /*  $("#templateSnapshot").fileinput({
-     allowedFileExtensions:['jpg', 'gif', 'png'],
-     allowedPreviewTypes:['images'],
-     maxFileSize:4096,
-     minFileCount:1,
-     maxFileCount:4
-     });
-     $("#templateResource").fileinput({ allowedFileExtensions:['rar', 'zip'],
-     maxFileSize:15360,
-     minFileCount:1,
-     maxFileCount:1});*/
-    var snapTemplateImage = WebUploader.create({
-        swf: "../js/webuploader/Uploader.swf",
-        // 选择文件的按钮。可选。
+    var templateSnapshotUploader=WebUploader.create({
+        // swf文件路径
+        swf : '../js/webuploader/Uploader.swf',
+        server : "./uploadSnapshotPic.action",
         // 内部根据当前运行是创建，可能是input元素，也可能是flash.
-        pick: '#templateSnapshot'
+        pick : {
+            id : '#templateSnapshotBtn',
+            multiple : true
+        },
+        // 只允许选择图片文件。
+        accept : {
+            title : 'Images',
+            extensions : 'gif,jpg,jpeg,bmp,png',
+            mimeTypes : 'image/*'
+        },
+        method : 'post',
+        fileVal : 'templateSnapshotPics',
+        thumb : {
+            width : 110,
+            height : 110,
+            // 图片质量，只有type为`image/jpeg`的时候才有效。
+            quality : 70,
+            // 是否允许放大，如果想要生成小图的时候不失真，此选项应该设置为false.
+            allowMagnify : true,
+            // 是否允许裁剪。
+            crop : true,
+            // 为空的话则保留原有图片格式。
+            // 否则强制转换成指定的类型。
+            type : 'image/jpeg'
+        },
+        auto : false,
+        // 不压缩image, 默认如果是jpeg，文件上传前会压缩一把再上传！
+        resize : false
     });
-    var templateZipResource = WebUploader.create({
-        swf: "../js/webuploader/Uploader.swf",
-        // 选择文件的按钮。可选。
-        // 内部根据当前运行是创建，可能是input元素，也可能是flash.
-        pick: '#templateResource'
-    });
+    templateSnapshotUploader.on("",function(){});
     //验证表单提交
     $("#templateAddForm").validate({
         rules: {
@@ -59,11 +71,11 @@ $(function () {
     });
     $("#submitTemplateBtn").click(function () {
         $("#templateAddForm").ajaxForm(function (data) {
-            var result = "";
-            if (data.isSuccess) {
-                result = "新增模板成功！";
-            } else {
-                result = "新增模板失败！";
+            var result="";
+            if(data.isSuccess){
+                result="新增模板成功！";
+            }else{
+                result="新增模板失败！";
             }
             $("#add_template_result").html(result);
             $("#add_template_modal").modal({keyboard: false});
